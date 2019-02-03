e62b0514de047b7c25574cc66d6e777927ccff34
Alexey Popov <6425762@gmail.com>
2019-02-03 20:54:05 +0300

    Golint fixes

--- goozzle.go
+++ goozzle.go
@@ -7,18 +7,27 @@ import (
 )
 
 const (
-	Version                   = 1
-	UserAgentChrome           = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-	UserAgentSafari           = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7"
-	UserAgentFirefox          = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0"
+	// Version goozzle version
+	Version = 1
+	// UserAgentChrome if you want to pretend to be Сhrome
+	UserAgentChrome = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+	// UserAgentSafari if you want to pretend to be Safari
+	UserAgentSafari = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7"
+	// UserAgentFirefox if you want to pretend to be Firefox
+	UserAgentFirefox = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0"
+	// UserAgentInternetExplorer if you want to pretend to be IE
 	UserAgentInternetExplorer = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"
-	UserAgentEdge             = "Mozilla/5.0 (Windows IoT 10.0; Android 6.0.1; WebView/3.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Mobile Safari/537.36 Edge/17.17083"
+	// UserAgentEdge if you want to pretend to be Edge
+	UserAgentEdge = "Mozilla/5.0 (Windows IoT 10.0; Android 6.0.1; WebView/3.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Mobile Safari/537.36 Edge/17.17083"
 )
 
+// UserAgentDefault default goozzle user agent
 var UserAgentDefault = fmt.Sprintf("Goozzle/%d", Version)
 
-type DebugRequestHandler func(*Response)
+// DebugHandler handler function to debug requests
+type DebugHandler func(*Response)
 
+// New returns request with custom method
 func New(method string, url *url.URL) *Request {
 	header := http.Header{}
 	header.Add("User-Agent", UserAgentDefault)
@@ -30,18 +39,22 @@ func New(method string, url *url.URL) *Request {
 	}
 }
 
+// Get creates GET request
 func Get(url *url.URL) *Request {
 	return New(http.MethodGet, url)
 }
 
+// Put creates PUT request
 func Put(url *url.URL) *Request {
 	return New(http.MethodPut, url)
 }
 
+// Post creates POST request
 func Post(url *url.URL) *Request {
 	return New(http.MethodPost, url)
 }
 
+// Delete creates DELETE request
 func Delete(url *url.URL) *Request {
 	return New(http.MethodDelete, url)
 }
